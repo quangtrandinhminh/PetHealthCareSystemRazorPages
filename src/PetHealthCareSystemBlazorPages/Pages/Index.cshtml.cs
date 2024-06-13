73858ab3ec9a5bf8ba1593fa7dfc65a69a5139d3
quangtrandinhminh <trandinhminhquangsxp@gmail.com>
2024-06-13 22:38:16 +0700

    Change namespace

--- src/PetHealthCareSystemBlazorPages/Pages/Index.cshtml.cs
+++ src/PetHealthCareSystemBlazorPages/Pages/Index.cshtml.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace PetHealthCareSystemBlazorPages.Pages
+namespace PetHealthCareSystemRazorPages.Pages
 {
     public class IndexModel : PageModel
     {
