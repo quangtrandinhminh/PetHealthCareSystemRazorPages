e297d7eb1e1ee34ff93ab74d88ebc23dd6ffa2b8
SE171507_Duy Anh <anhbdse171507@fpt.edu.vn>
2024-06-15 00:17:12 +0700

    Add: PetManageMent: Index, Create, Update, Delete

---
+++ src/PetHealthCareSystemBlazorPages/Pages/PetManagementPage/Details.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using BusinessObject.Entities;
+using DataAccessLayer;
+
+namespace PetHealthCareSystemRazorPages.Pages.PetManagementPage
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly DataAccessLayer.AppDbContext _context;
+
+        public DetailsModel(DataAccessLayer.AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public Pet Pet { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var pet = await _context.Pets.FirstOrDefaultAsync(m => m.Id == id);
+            if (pet == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                Pet = pet;
+            }
+            return Page();
+        }
+    }
+}
---
+++ src/PetHealthCareSystemBlazorPages/Pages/PetManagementPage/Index.cshtml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using BusinessObject.Entities;
+using Service.IServices;
+using Service.Services;
+using Microsoft.Extensions.DependencyInjection;
+using System.Collections.Generic;
+using BusinessObject.DTO.Pet;
+
+namespace PetHealthCareSystemRazorPages.Pages.PetManagementPage
+{
+    public class IndexModel : PageModel
+    {
+        private readonly IPetService _petService;
+
+        public IndexModel(IPetService petService)
+        {
+            _petService = petService;
+        }
+
+        public IList<PetResponseDto> Pet { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Pet = await _petService.GetAllPetsForCustomerAsync(2002);
+            
+        }
+    }
+}
