e8ee1ff1d6c1be123c31e59658276d3706d49057
DAB1602 <anhbdse171507@fpt.edu.vn>
2024-07-03 13:08:09 +0700

    Fix: transaction create error / update: css BookingForm, TransactionForm

---
+++ src/PetHealthCareSystemBlazorPages/Pages/Customer/AppointmentManagement/AppointmentList.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PetHealthCareSystemRazorPages.Pages.Customer.AppointmentManagement
+{
+    public class AppointmentListModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
