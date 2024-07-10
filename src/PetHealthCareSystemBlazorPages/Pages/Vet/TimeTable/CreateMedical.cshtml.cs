55916a0bba6c8f859e24255b7dc1c8dde3e4d313
Son Le <lehoangson2259@gmail.com>
2024-07-10 08:46:17 +0700

    make medirecord from appoint (demo)

--- src/PetHealthCareSystemBlazorPages/Pages/Vet/TimeTable/CreateMedical.cshtml.cs
+++ src/PetHealthCareSystemBlazorPages/Pages/Vet/TimeTable/CreateMedical.cshtml.cs
@@ -14,6 +14,7 @@ using Azure;
 using Microsoft.Identity.Client;
 using Repository.Extensions;
 using Service.Services;
+using BusinessObject.DTO.Pet;
 
 namespace PetHealthCareSystemRazorPages.Pages.Vet.TimeTable
 {
@@ -23,23 +24,42 @@ namespace PetHealthCareSystemRazorPages.Pages.Vet.TimeTable
         private readonly IMedicalService _medical;
         private readonly ITransactionService _transactionService;
         private readonly IAppointmentService _appointment;
+        private readonly IMedicalItemService _medicalItem;
         [BindProperty]
         public AppointmentResponseDto AppointmentItem { get; set; }
-        public CreateMedicalModel(DataAccessLayer.AppDbContext context, IMedicalService medical, ITransactionService transactionService, IAppointmentService appointment)
+        [BindProperty]
+        public MedicalRecordRequestDto MedicalRecord { get; set; } = default!;
+        public CreateMedicalModel(DataAccessLayer.AppDbContext context, IMedicalService medical, ITransactionService transactionService, IAppointmentService appointment
+            , IMedicalItemService medicalItem)
         {
             _context = context;
             _medical = medical;
             _transactionService = transactionService;
             _appointment = appointment;
+            _medicalItem = medicalItem;
         }
 
         public async Task<IActionResult> OnGetAsync(int id)
         {
-            
+            var accountId = HttpContext.Session.GetString("UserId"); // Assuming UserId is stored in Session
+            var accountRole = HttpContext.Session.GetString("Role");
+            // Check if accountId is null or empty or if accountRole is not "admin" (assuming "admin" role is stored as such)
+            if (string.IsNullOrEmpty(accountId) || !IsVetRole(accountRole))
+            {
+                Response.Redirect("/Login");
+            }
             try
             {
                 var check = await _appointment.GetAppointmentByAppointmentId(id);
                 AppointmentItem = check;
+                var medicalItems = await _medicalItem.GetAllMedicalItem();
+                ViewData["PetId"] = new SelectList(AppointmentItem.Pets, "Id", "Name");
+                ViewData["MedicalItemsId"] = new SelectList(medicalItems, "Id", "Name");
+                MedicalRecord = new MedicalRecordRequestDto()
+                {
+                    AppointmentId = AppointmentItem.Id,
+                    
+                };
             }
             catch (Exception ex)
             {
@@ -51,29 +71,45 @@ namespace PetHealthCareSystemRazorPages.Pages.Vet.TimeTable
             return Page();
         }
 
-        [BindProperty]
-        public MedicalRecordRequestDto MedicalRecord { get; set; } = default!;
+        
 
         // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
         public async Task<IActionResult> OnPostAsync()
         {
             ModelState.Remove("Vet");
             ModelState.Remove("Pets");
+            ModelState.Remove("Customer");
             ModelState.Remove("Status");
             ModelState.Remove("Services");
             ModelState.Remove("Timetable");
             ModelState.Remove("BookingType");
-            if (!ModelState.IsValid && AppointmentItem != null)
+            if (!ModelState.IsValid)
             {
                 return Page();
             }
-            foreach (var pet in AppointmentItem.Pets)
+            try
             {
-                MedicalRecord.PetId = pet.Id;
-                await _medical.CreateMedicalRecord(MedicalRecord, AppointmentItem.Vet.Id);
-            }
+                var accountId = HttpContext.Session.GetString("UserId");
+                int id = int.Parse(accountId);
 
-            return RedirectToPage("./Index");
+                await _medical.CreateMedicalRecord(MedicalRecord, id);
+
+
+                return RedirectToPage("/Vet/TimeTable/Appointment");
+            }
+            catch (Exception e)
+            {
+                ModelState.AddModelError(string.Empty, e.Message);
+                return Page();
+            }
+            
+        }
+
+        private bool IsVetRole(string accountRole)
+        {
+            // Example check if "admin" is contained in the roles list
+            // Adjust this logic based on how roles are stored in your application
+            return !string.IsNullOrEmpty(accountRole) && accountRole.Split(',').Contains("Vet");
         }
     }
 }
