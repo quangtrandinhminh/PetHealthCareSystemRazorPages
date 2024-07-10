db714868ef579a61824ee1151cacbe3bb52aedc0
Son Le <lehoangson2259@gmail.com>
2024-07-10 08:00:54 +0700

    add paging appointment vet

--- src/PetHealthCareSystemBlazorPages/Pages/Vet/TimeTable/Appointment.cshtml.cs
+++ src/PetHealthCareSystemBlazorPages/Pages/Vet/TimeTable/Appointment.cshtml.cs
@@ -29,7 +29,7 @@ namespace PetHealthCareSystemRazorPages.Pages.Vet.TimeTable
         [BindProperty]
         public PaginatedList<AppointmentResponseDto> Appointment { get; set; } = default!;
 
-        public async Task<IActionResult> OnGetAsync(int? page)
+        public async Task<IActionResult> OnGetAsync(int? currentPage)
         {
             var accountId = HttpContext.Session.GetString("UserId"); // Assuming UserId is stored in Session
             var accountRole = HttpContext.Session.GetString("Role");
@@ -41,29 +41,17 @@ namespace PetHealthCareSystemRazorPages.Pages.Vet.TimeTable
             try
             {
 
-                int pagenumber = 1;
+                int pagenumber;
                 int pagesize = 3;
                 int id = int.Parse(accountId);
                 var date = DateTime.Now.ToString("yyyy-MM-dd");
-                //if (check == null)
-                //{
-                //    pagenumber = 2;
-                //}
-                //else if (check.Equals("next"))
-                //{
-                //    pagenumber = Appointment.PageNumber + 1;
-                //}
-                //else
-                //{
-                //    pagenumber = Appointment.PageNumber - 1;
-                //}
-                if (page == null)
+                if (currentPage == null)
                 {
-                    
+                    pagenumber = 1;
                 }
                 else
                 {
-                    pagenumber = (int)page;
+                    pagenumber = (int)currentPage;
                 }
                 var appoint = await _appointmentService.GetVetAppointmentsAsync(id, date, pagenumber, pagesize);
                 Appointment = appoint;
