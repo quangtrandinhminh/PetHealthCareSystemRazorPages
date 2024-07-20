ee39987b86e55a6ef361748e68a1b9773c26b756
SOL <lehoangson2259@gmail.com>
2024-07-20 11:50:02 +0700

    vet view hospitalization

---
+++ src/PetHealthCareSystemBlazorPages/Pages/Vet/TimeTable/Hospitalization.cshtml.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using BusinessObject.Entities;
+using DataAccessLayer;
+using Service.IServices;
+using BusinessObject.DTO.Appointment;
+using System.Configuration;
+using Repository.Extensions;
+using BusinessObject.DTO.User;
+using Azure;
+using Microsoft.Identity.Client;
+using BusinessObject.DTO.Hospitalization;
+
+namespace PetHealthCareSystemRazorPages.Pages.Vet.TimeTable
+{
+    public class HospitalizationModel : PageModel
+    {
+        private readonly IHospitalizationService _hospital;
+
+        public HospitalizationModel(IHospitalizationService hospitalization)
+        {
+            _hospital = hospitalization;
+        }
+
+        [BindProperty]
+        public PaginatedList<HospitalizationResponseDto> Hospitalize { get; set; } = default!;
+        public HospitalizationFilterDto HospitalizationFilter { get; set;}
+        [BindProperty(SupportsGet = true)]
+        public int PageSize { get; set; } = 5;
+        public async Task<IActionResult> OnGetAsync(int? currentPage)
+        {
+            var accountId = HttpContext.Session.GetString("UserId"); // Assuming UserId is stored in Session
+            var accountRole = HttpContext.Session.GetString("Role");
+            // Check if accountId is null or empty or if accountRole is not "admin" (assuming "admin" role is stored as such)
+            if (string.IsNullOrEmpty(accountId) || !IsVetRole(accountRole))
+            {
+                Response.Redirect("/Login");
+            }
+            try
+            {
+
+                int pagenumber;
+                int id = int.Parse(accountId);
+                var date = DateTime.Now.ToString("yyyy-MM-dd");
+
+                if (currentPage == null)
+                {
+                    pagenumber = 1;
+                }
+                else
+                {
+                    pagenumber = (int)currentPage;
+                }
+                var filter = new HospitalizationFilterDto
+                {
+                    VetId = id,
+                    Date = date,
+                };
+                var hos = await _hospital.GetAllHospitalizationWithFilters(filter, pagenumber, PageSize);
+                Hospitalize = hos;
+                return Page();
+                
+            }
+            catch (Exception ex)
+            {
+                Hospitalize = new PaginatedList<HospitalizationResponseDto>();
+                ModelState.AddModelError(string.Empty, ex.Message);
+                return Page();
+            }
+            
+        }
+        //public async Task OnPostNextPage() // Use OnPost to handle form submission
+        //{
+        //    string check = "next";
+        //    await OnGetAsync(check);
+        //}
+        private bool IsVetRole(string accountRole)
+        {
+            // Example check if "admin" is contained in the roles list
+            // Adjust this logic based on how roles are stored in your application
+            return !string.IsNullOrEmpty(accountRole) && accountRole.Split(',').Contains("Vet");
+        }
+    }
+}
