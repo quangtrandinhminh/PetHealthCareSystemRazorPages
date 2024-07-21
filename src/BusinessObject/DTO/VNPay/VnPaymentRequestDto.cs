ab580fa57986eb25b29a2dfc7529b1948d3cb9eb
quangtrandinhminh <trandinhminhquangsxp@gmail.com>
2024-07-22 04:28:41 +0700

    fix vnpay dto

--- src/BusinessObject/DTO/VNPay/VnPaymentRequestDto.cs
+++ src/BusinessObject/DTO/VNPay/VnPaymentRequestDto.cs
@@ -5,11 +5,10 @@ public class VnPaymentRequestDto
     public int OrderId { get; set; }
     public string FullName { get; set; }
 
-    // KH A thanh toán Appoiment hosp mr
+    //Tiếng Việt không dấu và không bao gồm các ký tự đặc biệt: Nap tien cho thue bao 0123456789. So tien 100,000 VND
     public string Description { get; set; }
     public double Amount { get; set; }
     public string VnPayCommand { get; set; }
     // 1 trang xử lý Vnpay
     public string ReturnUrl { get; set; }
-    public DateTime CreatedDate { get; set; }
 }
